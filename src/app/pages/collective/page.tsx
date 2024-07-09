18e7837c9bdb72bd52ca1520dbb82300660dc07f
Tyrone Henderson <Thender2022@gmail.com>
2024-07-09 13:15:27 -0700

    More structure

--- src/app/pages/collective/page.tsx
+++ src/app/pages/collective/page.tsx
@@ -1,7 +1,27 @@
-import "@/app/pages/main.css"
+import Image from "next/image";
+
 
 export default function CollectivePage() {
     return (
-        <h1>Explore our Collective</h1>
+        <div className="collective-page">
+            <div className="collective-page-title">
+                <h1>Explore our Collective</h1>
+            </div>
+            <div className="collective-page-img">
+                <Image 
+                    src="/grass.jpg"
+                    width={400}
+                    height={400}
+                    alt="Collective picture"
+                />
+            </div>
+            <div className="collective-artists-container">
+                <div className="featured-artists">
+                    <h1>Checkout the artists in our Collective</h1>
+                </div>
+
+            </div>
+        </div>
+        
     )
 }
--- src/app/pages/events/page.tsx
+++ src/app/pages/events/page.tsx
@@ -1,13 +1,32 @@
 export default function EventsPage() {
     return (
         <div className="event-page-container">
-            <div className="booking-container">
-                <h1>Book us for your Events!</h1>
-            </div>
-            <div className="booking-form">
-                
             
-            </div>
+            
+            <h1 className="mb-3 text-lg font-bold">Book us for your Events!</h1>
+            <form >
+                <input
+                    required
+                    name="name"
+                    placeholder="Name" 
+                    className="input-bordered input mb-2 w-full"
+                />
+                <textarea
+                    required
+                    name="description"
+                    placeholder="Description"
+                    className="textarea-bordered textarea mb-2 w-full"
+                />
+                <input
+                    required
+                    name="Duration"
+                    placeholder="3 Hour Minimum"
+                    type="url"
+                    className="textarea-bordered textarea mb-2 w-full"
+                />
+                
+            </form>
         </div>
+        
     )
 }
